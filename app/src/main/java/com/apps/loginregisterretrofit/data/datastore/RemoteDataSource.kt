34228da4f2cf4c0a756588c5388f761e2cda71e1
muhammadariefhidayat <tubagusarief45@gmail.com>
2022-07-03 04:50:23 +0700

    create remote data source

---
+++ app/src/main/java/com/apps/loginregisterretrofit/data/datastore/RemoteDataSource.kt
@@ -0,0 +1,18 @@
+package com.apps.loginregisterretrofit.data.datastore
+
+import android.util.Log
+import com.apps.loginregisterretrofit.data.api.ApiServices
+import com.apps.loginregisterretrofit.model.LoginBody
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class RemoteDataSource(private val apiServices: ApiServices) {
+    fun register(loginBody: LoginBody) = flow {
+        emit(apiServices.login2(loginBody))
+    }.catch {
+        Log.d("register", "register: failed = ${it.message}")
+    }.flowOn(Dispatchers.IO)
+
+}
---
+++ app/src/main/java/com/apps/loginregisterretrofit/data/repository/MainRepository.kt
@@ -0,0 +1,4 @@
+package com.apps.loginregisterretrofit.data.repository
+
+class MainRepository {
+}
