8482ec2de1ddd7b3e06fff59b46f9d929d8306b0
muhammadariefhidayat <tubagusarief45@gmail.com>
2022-07-03 04:54:41 +0700

    create class view model

--- app/src/main/java/com/apps/loginregisterretrofit/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/apps/loginregisterretrofit/ui/viewmodel/LoginViewModel.kt
@@ -2,11 +2,11 @@ package com.apps.loginregisterretrofit.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.apps.loginregisterretrofit.data.api.ApiServices
+import com.apps.loginregisterretrofit.data.repository.MainRepository
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class LoginViewModel:ViewModel() {
-
+class LoginViewModel(private val repository: MainRepository):ViewModel() {
 
 }
